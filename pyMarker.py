f3b5be5316ed64b4b9921c0e085a63595ca1e09c
sibo <siboster@gmail.com>
2020-08-21 14:46:15 -0400

    Update pyMarker.py

--- pyMarker.py
+++ pyMarker.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-A python program to watermark pdfs with array of names
+A python program to watermark pdfs with array of names. 
 0a) install python3 and pip3
-0b) install fpdf, PyPDF2, and Pillow libraries with pip: "pip install fpdf pypdf2 pillow"
-1) edit the "names" array
+0b) install fpdf, PyPDF2, and Pillow libraries with pip: "pip3 install fpdf pypdf2 pillow"
+1) edit the "names" array and the dpiRes (resolution)
 2) place this script in the same directory as your exam.pdf 
-3) run this program: "python3 pdfedit.py" (or just "python pdfedit.py" on some systems)
+3) run this program: "python3 pyMarker.py examName.pdf" or "python3 pyMarker.py examName.pdf text" (optional 'text' argument keeps text highlightable)
 """
 
 from fpdf import FPDF
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from pdf2image import convert_from_path
 from PIL import Image
-import os
+import os, sys
 
 names=["Sibo Lin","John Smith"]
-examPDF='exam.pdf'
 dpiRes=200
 
 def watermark(input_pdf, output_pdf, watermark_pdf):
@@ -33,7 +32,7 @@ def watermark(input_pdf, output_pdf, watermark_pdf):
         pdf_writer.write(fh)
 
 def pdf2jpg2pdf(input):
-    print("converting from pdf to graphic to pdf: " + input)       
+    print("converting from pdf to jpg to pdf: " + input)       
     pages = convert_from_path(input,dpi=dpiRes)
     imageList=[]
     prefix=os.path.splitext(input)[0]+'_'
@@ -46,33 +45,39 @@ def pdf2jpg2pdf(input):
         os.remove(prefix+str(i)+'.jpg')
     imageList[0].save(prefix+'img.pdf', save_all=True, append_images=imageList) 
 
-for name in names:
-    name += " "
-    repeated=name*20000
-    
-    pdf = FPDF()
-    pdf.add_page()
-    textsize=3
-    pdf.set_font("Helvetica",style='I',size=textsize)
-    pdf.set_text_color(0,0,0) 
-    pdf.write(h=textsize/3,txt=repeated)
-    #save watermark as PDF
-    pdf.output("watermark.pdf")
-    
-    #merge exam and watermark PDFs
-    output='exam_'+name.replace(" ","")+'.pdf'            
-    watermark(input_pdf=examPDF, 
-                  output_pdf=output,
-                  watermark_pdf='watermark.pdf')
-    os.remove('watermark.pdf')
-                  
-    """ optional: converts pdfs to graphics so that text is non-selectable. 
-    150dpi filesizes are ~4x larger
-    200dpi filesizes are ~8x larger
-    300dpi filesizes are ~20x larger. 
-    (Un)comment below lines to (de)activate
-    """             
-    pdf2jpg2pdf(output)
-    
-    #below line removes pdf with selectable text
-    os.remove(output)
+if __name__ == "__main__":
+    try: 
+        examPDF=sys.argv[1]
+        for name in names:
+            name += " "
+            repeated=name*20000
+            
+            pdf = FPDF()
+            pdf.add_page()
+            textsize=3
+            pdf.set_font("Helvetica",style='I',size=textsize)
+            pdf.set_text_color(0,0,0) 
+            pdf.write(h=textsize/3,txt=repeated)
+            #save watermark as PDF
+            pdf.output("watermark.pdf")
+            
+            #merge exam and watermark PDFs
+            output=examPDF[0:-4]+'_'+name.replace(" ","")+'.pdf'            
+            watermark(input_pdf=examPDF, 
+                          output_pdf=output,
+                          watermark_pdf='watermark.pdf')
+            os.remove('watermark.pdf')
+                          
+            """ unless "text" argument is given, convert pdfs to graphics so that text is non-selectable. 
+            150dpi filesizes are ~4x larger
+            200dpi filesizes are ~8x larger
+            300dpi filesizes are ~20x larger. 
+            """
+            if(sys.argv[-1] == "text"):
+              print("text in PDFs remain highlightable!")
+            else:             
+              pdf2jpg2pdf(output) 
+              #below line removes pdf with selectable text
+              os.remove(output)
+    except:
+      print("Error! .pdf file must be supplied in command. Examples:\n python3 pyMarker.py exam_chem.pdf \n python3 pyMarker.py exam_chem.pdf text")
