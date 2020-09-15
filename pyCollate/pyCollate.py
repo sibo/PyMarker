f37b720901940b059f0ecad2201cfb578134e2c1
sibo <siboster@gmail.com>
2020-09-14 21:23:31 -0400

    Update pyCollate.py

--- pyCollate/pyCollate.py
+++ pyCollate/pyCollate.py
@@ -32,9 +32,26 @@ def collate(output_pdf, nPages, key):
 
 if __name__ == "__main__":
 	try: 
-		output_pdf=sys.argv[1]
-		nPages=int(sys.argv[2])
-		key=sys.argv[3]
+		firstArg=sys.argv[1]
 	except:
-		print("Error! Three arguments must be supplied in commandline. Example:\n python3 pyCollate.py test.pdf 3 ab")
-	collate(output_pdf, nPages, key)
+		print("Error! The first command line argument must be a .pdf or .csv file. Examples: \"python3 pyCollate.py exams.csv 3\" or \"python3 pyCollate.py test.pdf 3 ab\"
+	if (firstArg[-4:]==".pdf"):
+		output_pdf = firstArg
+		try:
+			nPages=int(sys.argv[2])
+			key=sys.argv[3]
+		except:
+			print("Error! Three arguments must be supplied in commandline. Example:\n python3 pyCollate.py test.pdf 3 ab")
+		collate(output_pdf, nPages, key)
+    elif (firstArg[-4:]==".csv"):
+		try:
+			nPages=int(sys.argv[2])
+		except:
+			print("Error! Two arguments must be supplied in commandline. Examples: \"python3 pyCollate.py exams.csv 3\"")
+		import csv
+        with open(firstArg) as csv_file:
+            csv_reader = csv.reader(csv_file,delimiter=',')
+            for row in csv_reader:
+                collate(row[0]+".pdf",nPages,row[1])
+    else:
+        print("Error! The first command line argument must be a .pdf or .csv file. Examples: \"python3 pyCollate.py exams.csv 3\" or \"python3 pyCollate.py test.pdf 3 ab\"
